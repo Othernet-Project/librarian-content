aa888eff91bdfcac5efcd03a7e3a4b290db4910d
Andrean Franc <andrean.franc@gmail.com>
2015-09-29 18:39:30 +0200

    Allow with_content decorator to pass None to routes if required

--- librarian_content/decorators.py
+++ librarian_content/decorators.py
@@ -7,19 +7,23 @@ from librarian_content.library import metadata
 from librarian_content.library.archive import Archive
 
 
-def with_meta(func):
-    @functools.wraps(func)
-    def wrapper(path, **kwargs):
-        conf = request.app.config
-        archive = Archive.setup(conf['library.backend'],
-                                request.db.content,
-                                contentdir=conf['library.contentdir'],
-                                meta_filename=conf['library.metadata'])
-        content = archive.get_single(path)
-        if not content:
-            abort(404)
+def with_meta(abort_if_not_found=True):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(path, **kwargs):
+            conf = request.app.config
+            archive = Archive.setup(conf['library.backend'],
+                                    request.db.content,
+                                    contentdir=conf['library.contentdir'],
+                                    meta_filename=conf['library.metadata'])
+            content = archive.get_single(path)
+            if not content:
+                if abort_if_not_found:
+                    abort(404)
+                return func(path=path, meta=None, **kwargs)
 
-        content_path = os.path.join(archive.config['contentdir'], path)
-        meta = metadata.Meta(content, content_path)
-        return func(meta=meta, **kwargs)
-    return wrapper
+            content_path = os.path.join(archive.config['contentdir'], path)
+            meta = metadata.Meta(content, content_path)
+            return func(path=path, meta=meta, **kwargs)
+        return wrapper
+    return decorator
