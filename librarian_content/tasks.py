58b2199b19bc1caf91a8a9b83d0fbfb7ecc11fb2
Andrean Franc <andrean.franc@gmail.com>
2015-10-29 14:34:26 +0100

    Fix events api call

--- librarian_content/tasks.py
+++ librarian_content/tasks.py
@@ -19,4 +19,4 @@ def check_new_content(supervisor):
         if is_content(fsobj.path, config['library.metadata']):
             archive.add_to_archive(fsobj.path)
         else:
-            supervisor.events.publish('FILE_ADDED', fsobj)
+            supervisor.exts.events.publish('FILE_ADDED', fsobj)
