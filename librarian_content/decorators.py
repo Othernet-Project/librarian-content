8750d285b1c299311b7f89d3f83becd6bab0c7e2
Manish Mathai <manishpmathai@gmail.com>
2016-02-12 19:04:06 +0530

    Add `@with_facets` decorator

--- librarian_content/decorators.py
+++ librarian_content/decorators.py
@@ -5,6 +5,8 @@ from bottle_utils.html import urlunquote
 
 from librarian_content.library import metadata
 from librarian_content.library.archive import Archive
+from librarian_content.library.facets.facets import Facets
+from librarian_content.library.facets.archive import FacetsArchive
 
 
 def with_meta(abort_if_not_found=True):
@@ -27,5 +29,26 @@ def with_meta(abort_if_not_found=True):
                                  content.path,
                                  data=content)
             return func(path=path, meta=meta, **kwargs)
+        return wrapper
+    return decorator
+
+
+def with_facets(abort_if_not_found=True):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(path, **kwargs):
+            path = urlunquote(path)
+            conf = request.app.config
+            archive = FacetsArchive(request.app.supervisor.exts.fsal,
+                                    request.db.facets,
+                                    config=conf)
+            data = archive.get_facets(path)
+            if not data:
+                if abort_if_not_found:
+                    abort(404)
+                return func(path=path, facets=None, **kwargs)
+
+            facets = Facets(request.app.supervisor, data.path, data=data)
+            return func(path=path, facets=facets, **kwargs)
         return wrapper
     return decorator
