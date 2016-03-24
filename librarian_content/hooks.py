bbe8c32158c7b9bed7fe7d9111f9fc2ba7c876bd
Manish Mathai <manishpmathai@gmail.com>
2016-03-24 22:57:39 +0530

    Move facets scan scheduling from migration to post_start

--- librarian_content/hooks.py
+++ librarian_content/hooks.py
@@ -1,7 +1,11 @@
 from fsal.client import FSAL
 
 from .commands import refill_facets
-from .tasks import check_new_content
+from .tasks import check_new_content, scan_facets
+
+
+SCAN_DELAY = 5
+STEP_DELAY = 0.5
 
 
 def initialize(supervisor):
@@ -16,7 +20,15 @@ def initialize(supervisor):
 
 
 def post_start(supervisor):
-    refresh_rate = supervisor.config['facets.refresh_rate']
+    config = supervisor.config
+    refresh_rate = config['facets.refresh_rate']
     supervisor.exts.tasks.schedule(check_new_content,
                                    args=(supervisor, refresh_rate),
                                    delay=refresh_rate)
+    if config.get('facets.scan', False):
+        start_delay = config.get('facets.scan_delay', SCAN_DELAY)
+        step_delay = config.get('facets.scan_step_delay', STEP_DELAY)
+        kwargs = dict(step_delay=step_delay, config=config)
+        supervisor.exts.tasks.schedule(scan_facets,
+                                       kwargs=kwargs,
+                                       delay=start_delay)
--- librarian_content/migrations/facets/00_05_schedule_facets_scan.py
+++ librarian_content/migrations/facets/00_05_schedule_facets_scan.py
@@ -1,15 +1,2 @@
-from librarian_core.exts import ext_container as exts
-
-from ...tasks import scan_facets
-
-
-SCAN_DELAY = 5
-STEP_DELAY = 0.5
-
-
 def up(db, conf):
-    start_delay = conf.get('facets.scan_delay', SCAN_DELAY)
-    step_delay = conf.get('facets.scan_step_delay', STEP_DELAY)
-    exts.tasks.schedule(scan_facets,
-                        kwargs=dict(step_delay=step_delay, config=conf),
-                        delay=start_delay)
+    conf['facets.scan'] = True
